52493f1af039668c1ad4acf85734935e2994c77c
remycode <remycode33@gmail.com>
2023-02-13 21:49:17 +0100

    créer un schémas de données

---
+++ models/Thing.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+// on créait notre schémas de données grace à notre module mongoose.
+const thingSchema = mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  userId: { type: String, required: true },
+  price: { type: Number, required: true },
+});
+
+module.exports = mongoose.model('Thing', thingSchema);
