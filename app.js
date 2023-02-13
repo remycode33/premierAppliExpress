a9fbaaace9caf12da3ec382700710190459b29ab
remycode <remycode33@gmail.com>
2023-02-13 19:17:12 +0100

    fin PARTIE 1

--- app.js
+++ app.js
@@ -2,12 +2,21 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());//permet de recupérer la requet ensuite (Express prendra toutes les req qui ont Content-type : application/json et met à dispo leur body dans l'obj req)
+
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
 });
+
+app.post('/api/stuff', (req, res, next) => {
+    console.log(req.body);
+    res.status(201).json({
+      message: 'Objet créé !'
+    });
+  });
 
 app.use('/api/stuff', (req, res, next) => {
     const stuff = [
