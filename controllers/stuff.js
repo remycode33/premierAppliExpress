14cf866794b13a6c4996d8b81700ee3debf268f9
remycode <remycode33@gmail.com>
2023-02-14 10:43:25 +0100

    modification de la route PUT pour prendre en compte les fichiers

--- controllers/stuff.js
+++ controllers/stuff.js
@@ -32,27 +32,25 @@ exports.getOneThing = (req, res, next) => {
 };
 
 exports.modifyThing = (req, res, next) => {
-  const thing = new Thing({
-    _id: req.params.id,
-    title: req.body.title,
-    description: req.body.description,
-    imageUrl: req.body.imageUrl,
-    price: req.body.price,
-    userId: req.body.userId
-  });
-  Thing.updateOne({_id: req.params.id}, thing).then(
-    () => {
-      res.status(201).json({
-        message: 'Thing updated successfully!'
+  const thingObject = req.file ? {
+      ...JSON.parse(req.body.thing),
+      imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+  } : { ...req.body };
+
+  delete thingObject._userId;
+  Thing.findOne({_id: req.params.id})
+      .then((thing) => {
+          if (thing.userId != req.auth.userId) {
+              res.status(401).json({ message : 'Not authorized'});
+          } else {
+              Thing.updateOne({ _id: req.params.id}, { ...thingObject, _id: req.params.id})
+              .then(() => res.status(200).json({message : 'Objet modifié!'}))
+              .catch(error => res.status(401).json({ error }));
+          }
+      })
+      .catch((error) => {
+          res.status(400).json({ error });
       });
-    }
-  ).catch(
-    (error) => {
-      res.status(400).json({
-        error: error
-      });
-    }
-  );
 };
 
 exports.deleteThing = (req, res, next) => {
